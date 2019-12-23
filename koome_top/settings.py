5ade511b92ae820b571d43a6710797e3346471e7
komerela <kmwongera@icloud.com>
2019-12-22 22:28:05 -0800

    Changed path

--- koome_top/settings.py
+++ koome_top/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'portfolio_bs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['/koome.github.io/koome_top/templates/'],
+        'DIRS': ['/komerela.github.io/koome_top/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
